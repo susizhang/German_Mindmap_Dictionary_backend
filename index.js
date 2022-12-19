9a74b4c83f984ead3b525326d476d330d3f8a844
suqun zhang <susizhang2017@gmail.com>
2022-12-19 22:56:38 +0100

    cheerio.arr

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const wordsRouter = require("./routes/wordsRoutes");
 const app = express();
+const cors = require("cors");
 
 const port = process.env.PORT || 5000;
 app.use(express.json());
-
+app.use(cors());
 app.use("/", wordsRouter);
 app.listen(port, () =>
   console.log(`Server is running on  http://localhost:${port}`)
--- middleWare/searchedWord.js
+++ middleWare/searchedWord.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const cheerio = require("cheerio");
 
 const findSearchedWord = (req, res, next) => {
   const {
@@ -12,8 +13,23 @@ const findSearchedWord = (req, res, next) => {
     .then((response) => {
       const pages = response.data.query.pages;
       const page = Object.values(pages)[0];
-      console.log(page);
-      res.send(page.extract);
+      const $ = cheerio.load(page.extract);
+      $.html();
+      const bedeutungen = $("p[title='Sinn und Bezeichnetes (Semantik)']")
+        .next()
+        .html();
+
+      const bedeutungenToArr = $(bedeutungen)
+        .map(function (i, el) {
+          // this === el
+          return $(this).text();
+        })
+        .toArray();
+      // .join(" ")
+
+      res.send({
+        bedeutungen: bedeutungenToArr,
+      });
     });
 
   next();

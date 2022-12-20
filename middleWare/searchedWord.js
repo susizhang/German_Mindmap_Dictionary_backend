2b2f8cb02b4656e79797276f5f20d45d3c445413
suqun zhang <susizhang2017@gmail.com>
2022-12-20 11:27:51 +0100

    Adding beispiele and other data types

--- middleWare/searchedWord.js
+++ middleWare/searchedWord.js
@@ -13,8 +13,15 @@ const findSearchedWord = (req, res, next) => {
     .then((response) => {
       const pages = response.data.query.pages;
       const page = Object.values(pages)[0];
+
+      // use cheerio to parse html to json
+
       const $ = cheerio.load(page.extract);
       $.html();
+
+      // Get words
+
+      // Get bedeutungen data
       const bedeutungen = $("p[title='Sinn und Bezeichnetes (Semantik)']")
         .next()
         .html();
@@ -28,8 +35,113 @@ const findSearchedWord = (req, res, next) => {
       const filteredBedeutungenToArr = bedeutungenToArr.filter((x) => {
         return x !== "\n";
       });
+
+      // Get Synonyme
+
+      const synonyme = $("p[title='bedeutungsgleich gebrauchte Wörter']")
+        .next()
+        .html();
+
+      const synonymeToArr = $(synonyme)
+        .map(function (i, el) {
+          // this === el
+          return $(this).text();
+        })
+        .toArray();
+      const filteredSynonymeToArr = synonymeToArr.filter((x) => {
+        return x !== "\n";
+      });
+
+      // Get Sinnverwandte Wörter
+
+      const sinnverwandteWörter = $("p[title='Sinnverwandte Wörter']")
+        .next()
+        .html();
+
+      const sinnverwandteWörterToArr = $(sinnverwandteWörter)
+        .map(function (i, el) {
+          // this === el
+          return $(this).text();
+        })
+        .toArray();
+      const filteredSinnverwandteWörterToArr = sinnverwandteWörterToArr.filter(
+        (x) => {
+          return x !== "\n";
+        }
+      );
+
+      // Get Gegenwörter
+
+      const gegenwörter = $("p[title='Antonyme']").next().html();
+
+      const gegenwörterToArr = $(gegenwörter)
+        .map(function (i, el) {
+          // this === el
+          return $(this).text();
+        })
+        .toArray();
+      const filteredGegenwörterToArrToArr = gegenwörterToArr.filter((x) => {
+        return x !== "\n";
+      });
+
+      // Get Unterbegriffe
+
+      const unterbegriffe = $("p[title='Hyponyme']").next().html();
+
+      const unterbegriffeToArr = $(unterbegriffe)
+        .map(function (i, el) {
+          // this === el
+          return $(this).text();
+        })
+        .toArray();
+      const filteredUnterbegriffeToArr = unterbegriffeToArr.filter((x) => {
+        return x !== "\n";
+      });
+
+      // Get Beispiele
+
+      const beispiele = $("p[title='Verwendungsbeispielsätze']").next().html();
+
+      const beispieleToArr = $(beispiele)
+        .map(function (i, el) {
+          // this === el
+          return $(this).text();
+        })
+        .toArray();
+      const filteredBeispieleToArr = beispieleToArr.filter((x) => {
+        return x !== "\n";
+      });
+
+      // Get Wortbildungen
+
+      const wortbildungen = $("p[title='Derivate, Komposita und Konversionen']")
+        .next()
+        .html();
+
+      const wortbildungenToArr = $(wortbildungen)
+        .map(function (i, el) {
+          // this === el
+          return $(this).text();
+        })
+        .toArray();
+      const filteredWortbildungenToArr = wortbildungenToArr.filter((x) => {
+        return x !== "\n";
+      });
+
       res.send({
         bedeutungen: filteredBedeutungenToArr,
+
+        beispiele: filteredBeispieleToArr,
+
+        synonyme: filteredSynonymeToArr,
+
+        sinnverwandteWörter: filteredSinnverwandteWörterToArr,
+
+        gegenwörter: filteredGegenwörterToArrToArr,
+
+        unterbegriffe: filteredUnterbegriffeToArr,
+
+        wortbildungen: filteredWortbildungenToArr,
       });
     });
 
